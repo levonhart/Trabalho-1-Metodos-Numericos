4805f55752be8b78dcd5f8237e840be29918ccb0
Leonardo de Abreu <leonardomkflush@gmail.com>
2018-09-30 19:44:26 -0300

    metodo da secante adicionado

--- include/raizes.h
+++ include/raizes.h
@@ -3,5 +3,6 @@
 
 double raiz_newton(double (*func)(double), double (*deriv)(double), double x0, double lim_erro, int maxIter);
 
+double raiz_secante(double (*func)(double), double x0, double x1, double lim_erro, int maxIter);
 
 #endif /* end of include guard: RAIZES_H_GSLVRNKE */
--- src/raizes.c
+++ src/raizes.c
@@ -13,3 +13,16 @@ double raiz_newton(double (*func)(double), double (*deriv)(double), double x0, d
 	return x0;
 
 }
+
+double raiz_secante(double (*func)(double), double x0, double x1, double lim_erro, int maxIter){
+	double Fx0 = func(x0), Fx1 = func(x1);
+	printf("(%d)\t%lf\t%lf\n",0,x1,Fx1);
+	if (fabs(Fx0) < lim_erro) return x0;
+	if (fabs(Fx1) < lim_erro) return x1;
+	for (int i = 0; fabs(Fx1) > lim_erro && i < maxIter; i++) {
+		x1 = x1 - Fx1/(Fx1-Fx0) * (x1-x0);
+		x0 = x1;
+		printf("(%d)\t%lf\t%lf\n",i+1,x1,Fx1);
+	}
+	return x1;
+}
